a0b1c3c749f6cd3186e2d388d56d3b1feadefea3
Rajashekhar V <rajashekharv.rymec@gmail.com>
2023-03-09 10:24:39 +0000

    Build Super Over League using JSX syntax by ReactJS

---
+++ index.js
@@ -0,0 +1,19 @@
+const element = (
+  <div class="bg-Container">
+    <h1 class="heading">Super Over League</h1>
+    <div class="image-Container">
+      <img
+        src="https://assets.ccbp.in/frontend/react-js/rcb-img.png"
+        class="image"
+        alt="rcb logo"
+      />
+      <img
+        src="https://assets.ccbp.in/frontend/react-js/csk-img.png"
+        class="image"
+        alt="csk logo"
+      />
+    </div>
+  </div>
+);
+
+ReactDOM.render(element, document.getElementById("root"));
